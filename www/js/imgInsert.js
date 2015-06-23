c13de628db8b2dc98d021829b7f68a482b45f7f5
Jono <jono@oclocaladmins-MacBook-Pro.local>
2015-06-23 21:41:20 +0100

    no message

--- www/js/imgInsert.js
+++ www/js/imgInsert.js
@@ -8,21 +8,39 @@ var list = document.getElementById("banner_area");
 	var elementChildren = list.children;
 	for (var i = 0; i < list.children.length; i++) {
 	    
-	var windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
-	var windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+	var windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	var windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 	var windowHeightString = windowHeight.toString() + "px";
+	var windowWidthString = windowWidth.toString() + "px";
 
 	var imgNaturalHeight = list.children[i].naturalHeight;
 	var imgNaturalWidth = list.children[i].naturalWidth;
-	var imgRatio = imgNaturalHeight/imgNaturalWidth;
+	var imgNaturalRatio = imgNaturalHeight/imgNaturalWidth;
 
-	if (windowWidth)
+	var widthCompare = windowWidth / imgNaturalWidth;
+	var heightCompare = windowHeight / imgNaturalHeight;
 
+	if (widthCompare >= heightCompare) {
 
+		var imgNewHeight = imgNaturalRatio * windowWidth;
+		imgNewHeight = Math.round(imgNewHeight);
+		imgNewHeight = imgNewHeight.toString() + "px";
+
+		list.children[i].style.width = windowWidthString;
+		list.children[i].style.height = imgNewHeight;
+	}
+
+	else if (heightCompare > widthCompare) {
+		var imgNewWidth = windowHeight / imgNaturalRatio;
+		imgNewWidth = Math.round(imgNewWidth);
+		imgNewWidth = imgNewWidth.toString() + "px";
+
+		list.children[i].style.height = windowHeightString;
+		list.children[i].style.width = imgNewWidth;
+	}
 	/*  list.style.height = windowHeight;
 	    list.style.width = imgNaturalWidth * imgRatio; */
-	    list.children[i].style.height = windowHeightString;
-	    list.children[i].style.width = imgNaturalWidth;
+
 
 	};
 };
